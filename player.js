370d774c9ae7e8e750a1f001c036220e0a437867
RehmanaliMomin <rehmanmomin@gmail.com>
2020-11-23 10:02:07 +0530

    fetching the jsonp and printing response

---
+++ player.js
@@ -0,0 +1,16 @@
+var url = 'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867';
+
+
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = 'https://guidedlearning.oracle.com/player/latest/static/css/stTip.css';
+document.head.appendChild(link);
+
+
+
+__5szm2kaj = function (data) {
+ 
+    console.log(data);
+
+}
